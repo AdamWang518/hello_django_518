3d339a19ad3ca7f40c00945625fe6fe1bf22016a
AdamWang518 <dodoga518@gmail.com>
2021-06-02 16:06:04 +0800

    new

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from hello.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',index),
+    path(r'', include('myhello.urls')),
+    path(r'', admin.site.urls),
 ]
---
+++ myhello/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+  
+from django.http.response import HttpResponse
+
+
+# Create your views here.
+def myIndex(request):
+    my_name=request.POST.get('name',"CGU")
+    return HttpResponse("Hello "+my_name)
