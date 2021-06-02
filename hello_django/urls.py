91e13686030c3d2781b0a91ea9bc1f07bb76e5bd
AdamWang518 <dodoga518@gmail.com>
2021-06-02 15:34:26 +0800

    index

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hello.views import myView
+from django.conf.urls import include
+from hello.views import index
 
 urlpatterns = [
-    path('',myView),
+    path('admin/', admin.site.urls),
+    path('',index),
 ]
